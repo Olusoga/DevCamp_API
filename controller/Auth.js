6b13d452305e9a810908ff2b132b5fe1cb1aa3c2
Shoga <57602445+Olusoga@users.noreply.github.com>
2022-08-08 09:49:35 +0100

    commit

--- controller/Auth.js
+++ controller/Auth.js
@@ -18,8 +18,43 @@ exports.register = asyncHandler(async (req, res, next)=>{
         password,
         role
     })
-    res.status(200).json({
-        success : true,
-        data:user
-    })
+    //create token
+
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({success:true, token});
+})
+//@desc   Login users
+//@route Get api/v1/auth/Login
+//@access public
+
+exports.login = asyncHandler(async (req, res, next) => {
+    const {email, password} = req.body;
+
+//validate email and matchedPassword
+
+if(!email||!password){
+   return next(new ErrorResponse('insert email and password', 400)) ;
+}
+
+//check for user
+   const user = await User.findOne({email}).select('+password')
+
+    if(!user) {
+        return next(new ErrorResponse('invalid credentials', 401))
+    }
+
+//check for matches
+const isMatch = await user.matchedPassword(password)
+
+if(!isMatch){
+    return next(new ErrorResponse('invalid credentials', 401))
+}else{
+    //create token
+
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({success:true, token});
+}
+
 })
--- models/User.js
+++ models/User.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
 const bcrypt = require('bcryptjs');
+
+const jwt = require("jsonwebtoken");
+
 const userSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -33,9 +36,20 @@ const userSchema = new mongoose.Schema({
     }
 })
 //encrypting the password
-userSchema.pre('save', async function(next){
+userSchema.pre('save', async function (next){
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt)
 
 });
+//get signed jwt and return
+userSchema.methods.getSignedJwtToken = function(){
+    return jwt.sign({id: this._id}, process.env.JWT_SECRET, {
+        expiresIn:process.env.JWT_EXPIRE
+    })
+}
+
+userSchema.methods.matchedPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
 module.exports = mongoose.model('User', userSchema);
